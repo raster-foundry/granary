177e5c353d91b0c27c14479fe2d524dd2a9dd342
James Santucci <james.santucci@gmail.com>
2019-10-16 10:42:30 -0500

    Add deps and docker-compose services

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,13 +4,14 @@ import sbt._
 
 // Versions
 object Versions {
-  val Http4sVersion    = "0.20.11"
-  val Specs2Version    = "4.1.0"
-  val LogbackVersion   = "1.2.3"
-  val ScapegoatVersion = "1.3.8"
-  val CirceVersion     = "0.12.1"
-  val DoobieVersion    = "0.8.2"
-  val TapirVersion     = "0.11.6"
+  val CirceVersion         = "0.12.1"
+  val DoobieVersion        = "0.8.2"
+  val Http4sVersion        = "0.20.11"
+  val LogbackVersion       = "1.2.3"
+  val RasterFoundryVersion = "1.31.0"
+  val ScapegoatVersion     = "1.3.8"
+  val Specs2Version        = "4.1.0"
+  val TapirVersion         = "0.11.6"
 }
 
 object Dependencies {
@@ -19,18 +20,19 @@ object Dependencies {
   val doobie                = "org.tpolecat"           %% "doobie-core"              % Versions.DoobieVersion
   val doobieHikari          = "org.tpolecat"           %% "doobie-hikari"            % Versions.DoobieVersion
   val doobiePostgres        = "org.tpolecat"           %% "doobie-postgres"          % Versions.DoobieVersion
-  val doobieSpecs2          = "org.tpolecat"           %% "doobie-specs2"            % Versions.DoobieVersion % "test"
   val doobieScalatest       = "org.tpolecat"           %% "doobie-scalatest"         % Versions.DoobieVersion % "test"
+  val doobieSpecs2          = "org.tpolecat"           %% "doobie-specs2"            % Versions.DoobieVersion % "test"
   val http4s                = "org.http4s"             %% "http4s-blaze-server"      % Versions.Http4sVersion
   val http4sCirce           = "org.http4s"             %% "http4s-circe"             % Versions.Http4sVersion
-  val http4sServer          = "org.http4s"             %% "http4s-blaze-server"      % Versions.Http4sVersion
   val http4sDsl             = "org.http4s"             %% "http4s-dsl"               % Versions.Http4sVersion
+  val http4sServer          = "org.http4s"             %% "http4s-blaze-server"      % Versions.Http4sVersion
   val logbackClassic        = "ch.qos.logback"         % "logback-classic"           % Versions.LogbackVersion
+  val rasterfoundryHttp4s   = "com.rasterfoundry"      %% "http4s-util"              % Versions.RasterFoundryVersion
   val specs2Core            = "org.specs2"             %% "specs2-core"              % Versions.Specs2Version % "test"
   val tapir                 = "com.softwaremill.tapir" %% "tapir-core"               % Versions.TapirVersion
   val tapirCirce            = "com.softwaremill.tapir" %% "tapir-json-circe"         % Versions.TapirVersion
   val tapirHttp4sServer     = "com.softwaremill.tapir" %% "tapir-http4s-server"      % Versions.TapirVersion
-  val tapirOpenAPIDocs      = "com.softwaremill.tapir" %% "tapir-openapi-docs"       % Versions.TapirVersion
   val tapirOpenAPICirceYAML = "com.softwaremill.tapir" %% "tapir-openapi-circe-yaml" % Versions.TapirVersion
+  val tapirOpenAPIDocs      = "com.softwaremill.tapir" %% "tapir-openapi-docs"       % Versions.TapirVersion
   val tapirSwaggerUIHttp4s  = "com.softwaremill.tapir" %% "tapir-swagger-ui-http4s"  % Versions.TapirVersion
 }
