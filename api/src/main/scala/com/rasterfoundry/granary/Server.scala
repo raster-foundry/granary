fd7c24b2af1da8fe4077689f09fdd2ac0f4ad1a3
Christopher Brown <cbrown@azavea.com>
2019-10-18 09:59:37 -0400

    Use Resource based Server Creation

--- api/src/main/scala/com/rasterfoundry/granary/Server.scala
+++ api/src/main/scala/com/rasterfoundry/granary/Server.scala
@@ -2,28 +2,26 @@ package com.rasterfoundry.granary.api
 
 import com.rasterfoundry.granary.api.endpoints._
 import com.rasterfoundry.granary.api.services._
-
 import cats.effect._
 import cats.implicits._
 import com.colisweb.tracing.TracingContext.TracingContextBuilder
 import com.rasterfoundry.http4s.{JaegerTracer, XRayTracer}
 import com.typesafe.scalalogging.LazyLogging
-import org.http4s._
 import org.http4s.implicits._
 import org.http4s.server.blaze._
 import org.http4s.server.middleware._
-import org.http4s.server.Router
+import org.http4s.server._
 import pureconfig.ConfigSource
 import pureconfig.error.ConfigReaderFailures
 import pureconfig.generic.auto._
-import tapir.openapi.OpenAPI
 import tapir.openapi.circe.yaml._
 import tapir.docs.openapi._
 import tapir.swagger.http4s.SwaggerHttp4s
+import cats.effect.implicits._
 
 object ApiServer extends IOApp with LazyLogging {
 
-  def getApp: Either[ConfigReaderFailures, HttpApp[IO]] =
+  def getTracingContextBuilder: Either[ConfigReaderFailures, TracingContextBuilder[IO]] =
     ConfigSource.default.at("tracing").load[TracingConfig] map {
       case TracingConfig(s) if s.toUpperCase() == "JAEGER" =>
         JaegerTracer.tracingContextBuilder
@@ -32,32 +30,31 @@ object ApiServer extends IOApp with LazyLogging {
       case TracingConfig(s) =>
         logger.warn(s"Not a recognized tracing sink: $s. Using Jaeger")
         JaegerTracer.tracingContextBuilder
-    } map { (contextBuilder: TracingContextBuilder[IO]) =>
-      implicit val tracingContextBuilder = contextBuilder
+    }
 
-      val allEndpoints              = HelloEndpoints.endpoints
-      val doc: OpenAPI              = allEndpoints.toOpenAPI("Granary", "0.0.1")
-      val docRoutes: HttpRoutes[IO] = new SwaggerHttp4s(doc.toYaml).routes
-      val helloRoutes               = (new HelloService).routes
-
-      CORS(
+  def createServer: Resource[IO, Server[IO]] =
+    for {
+      tracingContextBuilder <- Resource.liftF {
+        getTracingContextBuilder match {
+          case Left(e)        => IO.raiseError(throw new Exception(e.toString))
+          case Right(builder) => IO.pure(builder)
+        }
+      }
+      allEndpoints = HelloEndpoints.endpoints
+      docs         = allEndpoints.toOpenAPI("Granary", "0.0.1")
+      docRoutes    = new SwaggerHttp4s(docs.toYaml).routes
+      helloRoutes  = new HelloService(tracingContextBuilder).routes
+      router = CORS(
         Router(
           "/api" -> (helloRoutes <+> docRoutes)
         )
       ).orNotFound
-    }
+      server <- BlazeServerBuilder[IO]
+        .bindHttp(8080, "0.0.0.0")
+        .withHttpApp(router)
+        .resource
+    } yield server
 
   def run(args: List[String]): IO[ExitCode] =
-    getApp match {
-      case Right(httpApp) =>
-        BlazeServerBuilder[IO]
-          .bindHttp(8080, "0.0.0.0")
-          .withHttpApp(httpApp)
-          .serve
-          .compile
-          .drain
-          .as(ExitCode.Success)
-      case Left(e) =>
-        IO(println(s"Failed to initialize application: $e")).as(ExitCode.Error)
-    }
+    createServer.use(_ => IO.never).as(ExitCode.Success)
 }
