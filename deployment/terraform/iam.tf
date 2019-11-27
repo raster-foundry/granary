204857db1feb717ccae499a233b2b6285399d58e
James Santucci <james.santucci@gmail.com>
2019-11-27 12:05:17 -0500

    Attach s3 and batch full access to api tasks

--- deployment/terraform/iam.tf
+++ deployment/terraform/iam.tf
@@ -25,3 +25,13 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = var.aws_ecs_task_execution_role_policy_arn
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_s3" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_batch" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSBatchFullAccess"
+}
