feccf31e58cc066bb743a2cf118fcecfb09bd42d
Scala Steward <me@scala-steward.org>
2020-01-09 11:52:21 +0100

    Update tapir-core, tapir-http4s-server, ... to 0.12.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Versions {
   val ScapegoatVersion       = "1.3.11"
   val ScalacheckVersion      = "1.14.3"
   val Specs2Version          = "4.8.3"
-  val TapirVersion           = "0.12.12"
+  val TapirVersion           = "0.12.13"
 }
 
 object Dependencies {
