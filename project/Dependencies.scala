cf698cbb6625faf35ae974dedddeea3d7e23cb5c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-29 21:21:01 +0100

    Update specs2-core, specs2-scalacheck to 4.9.0 (#148)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Versions {
   val PureConfig             = "0.12.3"
   val ScapegoatVersion       = "1.3.11"
   val ScalacheckVersion      = "1.14.3"
-  val Specs2Version          = "4.8.3"
+  val Specs2Version          = "4.9.0"
   val TapirVersion           = "0.12.23"
 }
 
