b4616b707225bf6c62ea990a28a151a8f5581c73
Scala Steward <me@scala-steward.org>
2020-04-21 15:05:14 +0200

    Update flyway-core to 6.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Versions {
   val CirceVersion           = "0.13.0"
   val CirceJsonSchemaVersion = "0.1.0"
   val DoobieVersion          = "0.9.0"
-  val Flyway                 = "6.3.3"
+  val Flyway                 = "6.4.0"
   val Http4sVersion          = "0.21.3"
   val LogbackVersion         = "1.2.3"
   val Log4CatsVersion        = "1.0.1"
