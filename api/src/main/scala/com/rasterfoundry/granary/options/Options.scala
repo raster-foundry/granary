4a85d4283ea3408d50c2fa662906749302733fe4
James Santucci <james.santucci@gmail.com>
2020-06-01 15:55:27 -0500

    lint

--- api/src/main/scala/com/rasterfoundry/granary/options/Options.scala
+++ api/src/main/scala/com/rasterfoundry/granary/options/Options.scala
@@ -1,3 +1,8 @@
 package com.rasterfoundry.granary.api.options
 
-object Options extends DatabaseOptions with MetaOptions with S3Options with AuthOptions with PaginationOptions {}
+object Options
+    extends DatabaseOptions
+    with MetaOptions
+    with S3Options
+    with AuthOptions
+    with PaginationOptions {}
