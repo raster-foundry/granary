550c1d7f47151cf046d4a7c874123ff23bf522af
James Santucci <james.santucci@gmail.com>
2020-08-10 08:14:07 -0600

    map root to index

--- api/src/main/scala/com/rasterfoundry/granary/services/StaticService.scala
+++ api/src/main/scala/com/rasterfoundry/granary/services/StaticService.scala
@@ -7,12 +7,17 @@ import org.http4s.dsl.Http4sDsl
 class StaticService[F[_]: Sync](blocker: Blocker)(implicit cs: ContextShift[F])
     extends Http4sDsl[F] {
 
+  private val indexHtmlPath = "/assets/index.html"
+
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
+
+    case req @ GET -> Root =>
+      StaticFile.fromResource(indexHtmlPath, blocker, Some(req)).getOrElseF(NotFound())
 
     case req @ GET -> path =>
       StaticFile
         .fromResource(s"/assets$path", blocker, Some(req))
-        .orElse(StaticFile.fromResource("/assets/index.html", blocker, None))
+        .orElse(StaticFile.fromResource(indexHtmlPath, blocker, None))
         .getOrElseF(NotFound())
   }
 }
