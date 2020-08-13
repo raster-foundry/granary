6e6bde38dfacbd03c32a40ab34c2f55970d1cb19
James Santucci <james.santucci@gmail.com>
2020-08-13 16:51:39 -0600

    update execution generator

--- datamodel/src/test/scala/com/rasterfoundry/granary/Generators.scala
+++ datamodel/src/test/scala/com/rasterfoundry/granary/Generators.scala
@@ -22,7 +22,11 @@ trait Generators {
     }
 
   val executionGen: Gen[Execution.Create] =
-    (Gen.delay(UUID.randomUUID), Gen.const(Map.empty[String, String].asJson)).tupled map {
+    (
+      Gen.delay(UUID.randomUUID),
+      Gen.const(Map.empty[String, String].asJson),
+      shortStringGen
+    ).tupled map {
       Function.tupled(Execution.Create.apply)
     }
 
