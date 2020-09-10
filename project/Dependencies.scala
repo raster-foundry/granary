26ca6b7acfa76e695e375414b0272b235de17d37
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-10 19:19:22 +0200

    Update cats-effect to 2.2.0 (#419)
    * Update cats-effect to 2.2.0
    
    * Update cats-effect to 2.2.0
    
    Co-authored-by: James Santucci <james.santucci@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,9 @@ import sbt._
 
 // Versions
 object Versions {
+
   val awsSDK                 = "1.11.858"
-  val CatsEffectVersion      = "2.1.4"
+  val CatsEffectVersion      = "2.2.0"
   val CatsVersion            = "2.2.0"
   val CatsScalacheckVersion  = "0.3.0"
   val CirceVersion           = "0.13.0"
