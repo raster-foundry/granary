02de6540eadf8a73a6cd4937ae1f3cd0d51582c3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-09-11 16:28:23 +0200

    Update doobie-core, doobie-free, ... to 0.9.2 (#422)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Versions {
   val CirceVersion           = "0.13.0"
   val CirceJsonSchemaVersion = "0.1.0"
   val DeclineVersion         = "1.3.0"
-  val DoobieVersion          = "0.9.0"
+  val DoobieVersion          = "0.9.2"
   val Flyway                 = "6.5.5"
   val HikariVersion          = "3.4.5"
   val Http4sVersion          = "0.21.7"
