f20363be717921f178653064352d0a9e153fb37e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-29 22:53:22 +0100

    Update aws-java-sdk-batch, ... to 1.11.891 (#497)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 // Versions
 object Versions {
 
-  val awsSDK                 = "1.11.890"
+  val awsSDK                 = "1.11.891"
   val CatsEffectVersion      = "2.2.0"
   val CatsVersion            = "2.2.0"
   val CatsScalacheckVersion  = "0.3.0"
