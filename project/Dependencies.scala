f2d34ce4e5bfb1cc1634d66ecd7271e4ddf6332c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-12-01 11:23:28 +0100

    Update cats-core, cats-free, cats-kernel to 2.3.0 (#538)
    * Update cats-core, cats-free, cats-kernel to 2.3.0
    
    * Update cats-core, cats-effect, cats-free, ... to 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 object Versions {
 
   val awsSDK                 = "1.11.909"
-  val CatsEffectVersion      = "2.2.0"
-  val CatsVersion            = "2.2.0"
+  val CatsEffectVersion      = "2.3.0"
+  val CatsVersion            = "2.3.0"
   val CatsScalacheckVersion  = "0.3.0"
   val CirceVersion           = "0.13.0"
   val CirceJsonSchemaVersion = "0.1.0"
