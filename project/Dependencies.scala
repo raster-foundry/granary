b732baa300ba58cee3cd6a98fb1f387e55096154
Scala Steward <me@scala-steward.org>
2020-12-09 22:38:37 +0100

    Update aws-java-sdk-s3 to 1.11.916

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 // Versions
 object Versions {
 
-  val awsSDK                 = "1.11.915"
+  val awsSDK                 = "1.11.916"
   val CatsEffectVersion      = "2.3.0"
   val CatsVersion            = "2.3.0"
   val CatsScalacheckVersion  = "0.3.0"
