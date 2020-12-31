1b907235effcc77fff7a25c469217bb9f43d04e2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-01-01 00:51:30 +0100

    Update aws-java-sdk-core, aws-java-sdk-s3 to 1.11.930 (#579)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 // Versions
 object Versions {
 
-  val awsSDK                 = "1.11.929"
+  val awsSDK                 = "1.11.930"
   val CatsEffectVersion      = "2.3.1"
   val CatsVersion            = "2.3.1"
   val CatsScalacheckVersion  = "0.3.0"
