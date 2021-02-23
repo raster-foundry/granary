35ac2b67837b07fff113c9e65f943138e564989b
Scala Steward <me@scala-steward.org>
2021-02-23 02:47:11 +0100

    Update cats-core, cats-free, cats-kernel to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ import sbt._
 object Versions {
 
   val awsSDK                 = "1.11.960"
-  val CatsEffectVersion      = "2.3.1"
-  val CatsVersion            = "2.3.1"
+  val CatsEffectVersion      = "2.4.2"
+  val CatsVersion            = "2.4.2"
   val CatsScalacheckVersion  = "0.3.0"
   val CirceVersion           = "0.13.0"
   val CirceJsonSchemaVersion = "0.1.0"
