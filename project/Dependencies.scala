d688250bf3faf9bd7ea678e41db1b4eeca641e2a
Scala Steward <me@scala-steward.org>
2021-02-28 23:19:44 +0100

    Update pureconfig-core, ... to 0.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Versions {
   val MagnoliaVersion        = "0.16.0"
   val NewtypeVersion         = "0.4.4"
   val OpenTracingVersion     = "0.1.1"
-  val PureConfig             = "0.14.0"
+  val PureConfig             = "0.14.1"
   val RefinedVersion         = "0.9.21"
   val ScapegoatVersion       = "1.3.11"
   val ScalacheckVersion      = "1.15.3"
