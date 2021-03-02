736f56c53195fd4fdc5eb5f0f10f859f70e9f339
Scala Steward <me@scala-steward.org>
2021-03-02 17:10:20 +0100

    Update http4s-blaze-server, http4s-circe, ... to 0.21.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Versions {
   val Flyway                 = "7.5.4"
   val Fs2                    = "2.5.0"
   val HikariVersion          = "3.4.5"
-  val Http4sVersion          = "0.21.18"
+  val Http4sVersion          = "0.21.20"
   val LogbackVersion         = "1.2.3"
   val Log4CatsVersion        = "1.1.1"
   val MagnoliaVersion        = "0.16.0"
