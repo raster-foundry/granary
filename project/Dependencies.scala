4d34ed38cc09604aa1dffd3cf72de4bf145e4f3c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-04-08 16:50:41 +0200

    Update flyway-core to 7.7.3 (#739)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Versions {
   val CirceJsonSchemaVersion = "0.1.0"
   val DeclineVersion         = "2.0.0"
   val DoobieVersion          = "0.10.0"
-  val Flyway                 = "7.7.2"
+  val Flyway                 = "7.7.3"
   val Fs2                    = "2.5.4"
   val HikariVersion          = "3.4.5"
   val Http4sVersion          = "0.21.22"
